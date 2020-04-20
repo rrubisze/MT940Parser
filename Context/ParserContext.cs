b1c11a683bc8f22f54c39129c1704730555e6ca9
Rafał Rubiszewski <rafal.rubiszewski@edvantis.com>
2020-04-20 11:18:40 +0200

    Start working on report view, add context

--- Context/ParserContext.cs
+++ Context/ParserContext.cs
@@ -6,11 +6,16 @@ namespace MT940Parser.Context
 {
     public class ParserContext
     {
+        public event EventHandler ContextChanged;
         private Dictionary<string, object> dict = new Dictionary<string, object>();
         public object this[string i]
         {
             get => dict[i];
-            set => dict[i] = value;
+            set 
+            {
+                dict[i] = value;
+                ContextChanged.Invoke(this, null);
+            }
         }
     }
 }
--- ViewModels/ReportViewModel.cs
+++ ViewModels/ReportViewModel.cs
@@ -1,17 +1,53 @@
 ﻿using MT940Parser.Context;
+using MT940Parser.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text;
 
 namespace MT940Parser.ViewModels
 {
-    public class ReportViewModel: BaseViewModel
+    public class ReportViewModel: BaseViewModel, IDisposable
     {
-        private readonly ParserContext _context;
+        private ParserContext _context;
+        private ObservableCollection<Report> _reports;
+        private Summary _summary;
+
+        #region - PROPS -
+        public ObservableCollection<Report> Reports 
+        {
+            get => _reports;
+            set => Set(ref _reports, value);
+        }
+        public Summary Summary 
+        { 
+            get => _summary; 
+            set => Set(ref _summary, value); 
+        }
+        #endregion
 
         public ReportViewModel(ParserContext context)
         {
             _context = context;
+            _context.ContextChanged += ContextChanged;
         }
+
+        public void Dispose()
+        {
+            _context = null;
+        }
+
+        private void ContextChanged(object sender, EventArgs e)
+        {
+            var reports = _context["Reports"] as IEnumerable<Report>;
+            if(reports != null && reports.Count() > 0)
+            {
+                
+            }
+
+            this.Reports = new ObservableCollection<Report>(reports);
+        }
+
     }
 }
